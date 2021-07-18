65466b349028d73fa235d9d977844916031ba86e
pradhan-asish <pradhan.asish1994@gmail.com>
2021-07-18 21:17:07 +0530

    PDF Read Programs

---
+++ Python PDF/PDFFileReadandWrite.py
@@ -0,0 +1,13 @@
+import PyPDF2,os
+
+pdfFile = open('IPR.pdf','rb') ## to open PDF file
+reader = PyPDF2.PdfFileReader(pdfFile)
+print('Number of pages :', reader.numPages)
+page1 = reader.getPage(0)
+print()
+
+## TO read all the files of pdf
+for i in range(reader.numPages):
+    print('Page:'+str(i))
+    print(reader.getPage(i).extractText())
+
