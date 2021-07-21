ef579af8eec7015ba7333b2fc9088e3c79a5ba43
pradhan-asish <pradhan.asish1994@gmail.com>
2021-07-21 22:28:45 +0530

    Index wise access of list added

--- ListProgram.py
+++ ListProgram.py
@@ -3,3 +3,13 @@ print(myList)
 
 
 ## List is ordered, changeble, Allow duplicates
+
+
+##to access list item using index
+
+print(myList[1])
+
+for i in range(len(myList)):
+    print(myList[i])
+
+
