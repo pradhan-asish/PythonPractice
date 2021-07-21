df09ccc8705eddf70667f43da4381d6f14ead87a
pradhan-asish <pradhan.asish1994@gmail.com>
2021-07-21 22:32:45 +0530

    Accessing of elements using index

--- ListProgram.py
+++ ListProgram.py
@@ -1,4 +1,4 @@
-myList = ["Apple","Banana","Cherry"]
+myList = ["Apple","Banana","Cherry","Orange","Guava"]
 print(myList)
 
 
@@ -7,9 +7,16 @@ print(myList)
 
 ##to access list item using index
 
+print('+++++++++++++++++')
 print(myList[1])
 
+print('+++++++++++++++++')
 for i in range(len(myList)):
     print(myList[i])
 
+print('+++++++++++++++++')
+print(myList[2:5])
+print(myList[:2])
+print(myList[2:])
+print('+++++++++++++++++')
 
