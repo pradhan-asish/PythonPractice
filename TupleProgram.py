08e978e77a8e28bb1e63a104934086a64a9a3198
pradhan-asish <pradhan.asish1994@gmail.com>
2021-07-22 19:56:16 +0530

    Update of Tuple

--- TupleProgram.py
+++ TupleProgram.py
@@ -9,3 +9,13 @@ print('+++Using Indexes+++')
 print(myTuple[1])
 print(myTuple[1:3])
 print(myTuple[-2])
+
+## update Tuples
+print('+++Add of new element+++')
+y= list(myTuple)
+y.append("kiwi")
+myTuple = tuple(y)
+print(myTuple)
+
+
+
