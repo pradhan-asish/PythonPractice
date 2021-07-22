27b7a7e5e4b53768491456503a18a4d75ac5485e
pradhan-asish <pradhan.asish1994@gmail.com>
2021-07-22 19:45:49 +0530

    access of tuple

--- TupleProgram.py
+++ TupleProgram.py
@@ -1,4 +1,11 @@
 ################Tuples###############
 ##Ordered, Unchangeable, Allow Duplicates##
-myTuple = ("apple","banana","guava","orange")
+myTuple = ("apple","banana","guava","orange","papaya")
 print(myTuple)
+
+
+## Tuples
+print('+++Using Indexes+++')
+print(myTuple[1])
+print(myTuple[1:3])
+print(myTuple[-2])
