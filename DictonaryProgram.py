a8a37f222f491403a54ad6ee7213993f23e57ef5
pradhan-asish <pradhan.asish1994@gmail.com>
2021-07-24 01:01:54 +0530

    Update DictonaryProgram.py

--- DictonaryProgram.py
+++ DictonaryProgram.py
@@ -4,5 +4,11 @@ thisDict = {"brand" : "BMW",
             "model" : "Q3",
             "year" : 2001}
 
-print(thisDict)
 
+
+#########Dictionary are ordered after 3.7 version python prior to that it was unordered#########
+#########Dictionary are Changeable
+########Duplicates are not allowed
+print(thisDict)
+print('++++++How to access dictionary++++++')
+print(thisDict["brand"])
