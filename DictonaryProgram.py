2ad36ebe6ac13d391f61aa9973372692f6ce47ae
pradhan-asish <pradhan.asish1994@gmail.com>
2021-07-25 20:20:44 +0530

    Update DictonaryProgram.py

--- DictonaryProgram.py
+++ DictonaryProgram.py
@@ -47,4 +47,28 @@ print('++++++items copied++++++')
 copyDict = thisDict.copy()
 print(copyDict)
 
+##Nested dictionaries
+
+child1 = {
+  "name" : "Emil",
+  "year" : 2004
+}
+child2 = {
+  "name" : "Tobias",
+  "year" : 2007
+}
+child3 = {
+  "name" : "Linus",
+  "year" : 2011
+}
+
+myfamily = {
+  "child1" : child1,
+  "child2" : child2,
+  "child3" : child3
+}
+print('++++++Nested dictionaries++++++')
+
+print(myfamily)
+
 
